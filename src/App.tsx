08a3ec9d5d41971f883a45c1ce3a42399afcc537
Andre <andreddavies@hotmail.com>
2022-07-26 00:56:08 -0300

    Providing store for application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
 
+import store from "./store";
 import ThemeProviderWrapper from "./pages/ThemeProviderWrapper";
 
 function App() {
   return (
-    <ThemeProviderWrapper>
-      <div>
-        <h2>Gerenciador de Cupom</h2>
-      </div>
-    </ThemeProviderWrapper>
+    <ReduxProvider store={store}>
+      <ThemeProviderWrapper>
+        <div>
+          <h2>Gerenciador de Cupom</h2>
+        </div>
+      </ThemeProviderWrapper>
+    </ReduxProvider>
   );
 }
 
