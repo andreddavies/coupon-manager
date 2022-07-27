fde15d68abd4791c1f5a9a3d85c49d9f4fd92de5
Andre <andreddavies@hotmail.com>
2022-07-27 19:08:10 -0300

    Create basic markup for login page

---
+++ src/pages/Authentication/Login/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Login = (): React.ReactElement => {
+  return (
+    <S.Container>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </S.Container>
+  );
+};
+
+export default Login;
