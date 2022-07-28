b4d45f7d36d75cf11d79008316edc5a8deea1868
Andre <andreddavies@hotmail.com>
2022-07-27 23:16:05 -0300

    Updating page styles and components

--- src/pages/Authentication/Login/index.tsx
+++ src/pages/Authentication/Login/index.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 
+import Heading from "../../../components/Heading";
 import LoginForm from "../../../components/LoginForm";
+
+import { useDeviceDetect } from "../../../hooks/useDeviceDetect";
 
 import * as S from "./styles";
 
 const Login = (): React.ReactElement => {
+  const { isMobile } = useDeviceDetect();
+
   return (
     <S.Container>
-      <div>
-        <h1>Login</h1>
+      <S.TextWrapper>
+        <Heading
+          type="h2"
+          weight="500"
+          color="primary"
+          size={isMobile ? 1.2 : 1.8}
+        >
+          <>
+            Efetue seu <strong>login</strong>
+          </>
+        </Heading>
+      </S.TextWrapper>
 
-        <LoginForm />
-      </div>
+      <LoginForm />
     </S.Container>
   );
 };
--- src/pages/Authentication/Login/styles.ts
+++ src/pages/Authentication/Login/styles.ts
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  padding: 30px 0;
+  align-items: center;
+  flex-direction: column;
   background-color: ${({ theme }) => theme.background};
 `;
 
-export { Container };
+const TextWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+export { Container, TextWrapper };
