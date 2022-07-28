1face524162e5a4ea9e54a61235034d6eaac0093
Andre <andreddavies@hotmail.com>
2022-07-27 21:41:09 -0300

    Implementing header

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import RouterProvider from "./components/Providers/RouterProvider";
 import ThemeProviderWrapper from "./components/Providers/ThemeProviderWrapper";
+import Header from "./components/Header";
 
 function App() {
   return (
     <ReduxProvider store={store}>
       <BrowserRouter>
         <ThemeProviderWrapper>
+          <Header />
           <RouterProvider />
         </ThemeProviderWrapper>
       </BrowserRouter>
