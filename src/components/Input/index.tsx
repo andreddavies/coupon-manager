0f3a1f2d5519b4597b35adb408aa50572d37109a
Andre <andreddavies@hotmail.com>
2022-07-28 20:09:46 -0300

    Creating logic to validate input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,58 +1,93 @@
 import React, { useState } from "react";
 
 import * as S from "./styles";
+import { TInputVariation } from "./types";
+
+type TValidationCriteria = {
+  criterionRegExp: RegExp;
+  errorMessage: string;
+};
 
 type Props = {
   id: string;
   label: string;
   value: string;
   width?: string;
-  validationCriteria: string[];
+  minLength?: number;
+  maxLength?: number;
+  disabled?: boolean;
+  storeDispatch?: () => void;
   isValid: (value: boolean) => void;
+  validationCriteria: TValidationCriteria[];
+  type: "number" | "email" | "text" | "password" | "tel";
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const Input = ({
   id,
-  width,
+  type,
   label,
+  width,
   value,
   isValid,
   onChange,
+  minLength,
+  maxLength,
+  storeDispatch,
+  disabled = false,
   validationCriteria,
 }: Props): React.ReactElement => {
   const [focused, setFocused] = useState<boolean>(false);
-  const [messageText, setMessageText] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [variation, setVariation] = useState<TInputVariation>("initial");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleChangeCapture = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     onChange(event);
+    const errors: string[] = [];
 
-    isValid(true);
     validationCriteria.map((criterion) => {
-      console.log(criterion);
-      return criterion;
+      const { criterionRegExp, errorMessage } = criterion;
+      const status = criterionRegExp.test(event.target.value);
+
+      if (!status) errors.push(errorMessage);
+
+      return { status };
     });
 
-    setMessageText("Erro");
+    setVariation(errors.length === 0 ? "valid" : "invalid");
+    setErrorMessage(errors[0]);
+
+    isValid(variation === "valid");
+
+    if (storeDispatch && variation !== "invalid") {
+      storeDispatch();
+    }
   };
 
   return (
     <S.Container width={width}>
-      <S.InputContainer>
+      <S.InputContainer variation={variation}>
         <S.Label htmlFor={id} focused={focused}>
           {label}
         </S.Label>
 
         <S.Input
           id={id}
+          type={type}
           value={value}
-          onChange={handleChange}
+          disabled={disabled}
+          minLength={minLength}
+          maxLength={maxLength}
+          variation={variation}
+          onChange={handleChangeCapture}
           onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
+          onBlur={() => !Boolean(value.length) && setFocused(false)}
         />
       </S.InputContainer>
 
-      <S.MessageText>{messageText}</S.MessageText>
+      <S.MessageText>{errorMessage}</S.MessageText>
     </S.Container>
   );
 };
