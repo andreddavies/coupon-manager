ff16e3216de5ddef4a7b097dd2e274dd184d5f1f
Andre <andreddavies@hotmail.com>
2022-07-27 21:36:55 -0300

    Adding color schemes

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,5 +1,17 @@
 export const light = {
-  primary: "#5074b6", // light
-  secondary: "#164986", // medium
-  tertiary: "#002259", // dark
+  primary: "#5074b6",
+  secondary: "#164986",
+  tertiary: "#002259",
+  quaternary: "#292837",
+
+  header: {
+    background: "#5074b6",
+    shadow: "rgba(0, 0, 0, 0.2)",
+  },
+
+  heading: {
+    primary: "#000",
+    secondary: "#fff",
+    tertiary: "#817E7E",
+  },
 };
