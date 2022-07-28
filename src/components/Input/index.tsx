7ff417e2fdc7e9e96b51273e47c9fae8f2fc5029
Andre <andreddavies@hotmail.com>
2022-07-28 01:08:27 -0300

    Updating input props and styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import * as S from "./styles";
 
-const Input = (): React.ReactElement => {
+type Props = {
+  id: string;
+  label: string;
+  value: string;
+  width?: string;
+  validationCriteria: string[];
+  isValid: (value: boolean) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({
+  id,
+  width,
+  label,
+  value,
+  isValid,
+  onChange,
+  validationCriteria,
+}: Props): React.ReactElement => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [messageText, setMessageText] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    onChange(event);
+
+    isValid(true);
+    validationCriteria.map((criterion) => {
+      console.log(criterion);
+      return criterion;
+    });
+
+    setMessageText("Erro");
+  };
+
   return (
-    <S.Container>
-      <h1>Input</h1>
+    <S.Container width={width}>
+      <S.InputContainer>
+        <S.Label htmlFor={id} focused={focused}>
+          {label}
+        </S.Label>
+
+        <S.Input
+          id={id}
+          value={value}
+          onChange={handleChange}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
+      </S.InputContainer>
+
+      <S.MessageText>{messageText}</S.MessageText>
     </S.Container>
   );
 };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,59 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+type ContainerProps = {
+  width?: string;
+  marginVertical?: number;
+};
 
-const Input = styled.input``;
+type LabelProps = {
+  focused: boolean;
+};
 
-export { Container, Input };
+const Container = styled.div<ContainerProps>`
+  max-width: 582px;
+  position: relative;
+  width: ${({ width }) => (!width && "279px") || `${width}`};
+  margin: ${({ marginVertical }) =>
+    (marginVertical && `${marginVertical}rem 0`) || "0.75rem 0"};
+`;
+
+const InputContainer = styled.div`
+  width: 100%;
+  height: 56px;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.input.border};
+  box-shadow: 0 0 2px 1px ${({ theme }) => theme.input.shadow};
+  &:hover {
+    box-shadow: 0 0 8px 2px ${({ theme }) => theme.input.shadow};
+  }
+`;
+
+const Label = styled.label<LabelProps>`
+  transition: 0.1s;
+  margin-left: 8px;
+  font-size: 0.8rem;
+  position: absolute;
+  color: ${({ theme }) => theme.input.label};
+  top: ${({ focused }) => (!focused && "21.6px") || "2px"};
+`;
+
+const Input = styled.input`
+  outline: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  font-size: 1rem;
+  padding: 8px 10px;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.input.text};
+  background-color: ${({ theme }) => theme.input.background};
+`;
+
+const MessageText = styled.span`
+  margin-left: 8px;
+  font-weight: 600;
+  font-size: 0.625rem;
+  color: ${({ theme }) => theme.input.messageText};
+`;
+
+export { Container, InputContainer, Label, Input, MessageText };
