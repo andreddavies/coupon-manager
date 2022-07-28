1ae6dc62643ba5cac49d285d26648bc2b3181c2d
Andre <andreddavies@hotmail.com>
2022-07-28 01:08:55 -0300

    Implementing first version of input in login form

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "../Form";
+import Input from "../Input";
 
 import * as S from "./styles";
 
 const LoginForm = (): React.ReactElement => {
+  const [email, setEmail] = useState<string>("");
+
   return (
     <Form id="loginForm" onSubmit={() => console.log("olá")}>
-      <h1>LoginForm</h1>
+      <Input
+        id="email"
+        width="80%"
+        value={email}
+        label="E-mail"
+        validationCriteria={[""]}
+        isValid={(value) => console.log(value)}
+        onChange={(event) => setEmail(event.target.value)}
+      />
     </Form>
   );
 };
