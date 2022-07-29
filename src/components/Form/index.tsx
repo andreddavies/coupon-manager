028ff0436977e98750fdd46b9f18866614045189
Andre <andreddavies@hotmail.com>
2022-07-28 23:01:30 -0300

    Adding a disabled prop to form button

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,6 +5,7 @@ import * as S from "./styles";
 type Props = {
   id: string;
   buttonText?: string;
+  buttonDisabled: boolean;
   children: React.ReactElement | React.ReactElement[];
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 };
@@ -13,13 +14,16 @@ const Form = ({
   id,
   children,
   onSubmit,
+  buttonDisabled,
   buttonText = "Finalizar",
 }: Props): React.ReactElement => {
   return (
     <S.Container id={id} onSubmit={onSubmit}>
       {children}
 
-      <S.Button form={id}>{buttonText}</S.Button>
+      <S.Button form={id} disabled={buttonDisabled}>
+        {buttonText}
+      </S.Button>
     </S.Container>
   );
 };
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+type ButtonProps = {
+  disabled: boolean;
+};
+
 const Container = styled.form`
   width: 80%;
   display: flex;
@@ -17,28 +21,33 @@ const Container = styled.form`
   }
 `;
 
-const Button = styled.button`
+const Button = styled.button<ButtonProps>`
   border: 0;
   width: 80%;
   outline: 0;
   height: 40px;
+  margin: 1rem 0;
   font-weight: 600;
   max-width: 328px;
   font-size: 1.2rem;
   border-radius: 20px;
   color: ${({ theme }) => theme.form.button.text};
-  background-color: ${({ theme }) => theme.form.button.background};
+  background-color: ${({ theme, disabled }) =>
+    theme.form.button[disabled ? "disabled" : "background"]};
 
   &:hover {
-    background-color: ${({ theme }) => theme.form.button.hover};
+    background-color: ${({ theme, disabled }) =>
+      !disabled && theme.form.button.hover};
   }
 
   &:active {
-    background-color: ${({ theme }) => theme.form.button.active};
+    background-color: ${({ theme, disabled }) =>
+      !disabled && theme.form.button.active};
   }
 
   @media screen and (min-width: 1281px) {
     height: 50px;
+    margin: 2rem 0;
     border-radius: 30px;
   }
 `;
