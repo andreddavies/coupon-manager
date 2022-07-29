df2283f584b2fa791b87ca4e879d3ce1519c3729
Andre <andreddavies@hotmail.com>
2022-07-28 23:53:55 -0300

    Implementing register form on page and updating

--- src/pages/Authentication/Register/index.tsx
+++ src/pages/Authentication/Register/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import Heading from "../../../components/Heading";
+import RegisterForm from "../../../components/RegisterForm";
+
+import { useDeviceDetect } from "../../../hooks/useDeviceDetect";
+
 import * as S from "./styles";
 
 const Register = (): React.ReactElement => {
+  const { isMobile } = useDeviceDetect();
+
   return (
     <S.Container>
-      <div>
-        <h1>Register Page</h1>
-      </div>
+      <S.TextWrapper>
+        <Heading
+          type="h2"
+          weight="500"
+          color="primary"
+          size={isMobile ? 1.2 : 1.8}
+        >
+          <>
+            Efetue seu <strong>cadastro</strong>
+          </>
+        </Heading>
+      </S.TextWrapper>
+
+      <RegisterForm />
     </S.Container>
   );
 };
