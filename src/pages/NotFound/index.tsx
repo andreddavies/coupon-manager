cf3cb6ad5729991817f6d36c512182628e4b5e5b
Andre <andreddavies@hotmail.com>
2022-07-29 20:27:01 -0300

    Creating simple markup of not found page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const NotFound = (): React.ReactElement => {
+  return (
+    <S.Container>
+      <h1>Página não encontrada</h1>
+    </S.Container>
+  );
+};
+
+export default NotFound;
