a58f0e20a930feac14e7de8b198d6c093854e676
Andre <andreddavies@hotmail.com>
2022-07-28 23:00:05 -0300

    Adding register route

--- src/components/Providers/RouterProvider/index.tsx
+++ src/components/Providers/RouterProvider/index.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "../../../pages/Dashboard";
 import Login from "../../../pages/Authentication/Login";
+import Register from "../../../pages/Authentication/Register";
 import CreateCoupon from "../../../pages/Dashboard/CreateCoupon";
 
 const RouterProvider = (): React.ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/create-coupon" element={<CreateCoupon />} />
     </Routes>
