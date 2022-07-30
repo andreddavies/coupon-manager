fb815aabdebfd326e0c4a3d8689641da6ce287ab
Andre <andreddavies@hotmail.com>
2022-07-29 22:42:45 -0300

    Implementing settings page layout

--- src/pages/Dashboard/Settings/index.tsx
+++ src/pages/Dashboard/Settings/index.tsx
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import Heading from "../../../components/Heading";
+import UpdateAccountForm from "../../../components/UpdateAccountForm";
+
+import { RootState } from "../../../store";
+import { useDeviceDetect } from "../../../hooks/useDeviceDetect";
+
+import * as S from "./styles";
 
 const Settings = (): React.ReactElement => {
+  const router = useNavigate();
+  const { isMobile } = useDeviceDetect();
+  const store = useSelector((state: RootState) => state);
+
+  const handleDeleteAccount = async (): Promise<void> => {
+    await console.log("deleted");
+  };
+
+  useEffect(() => {
+    if (!store.auth.authenticated) {
+      router("/");
+    }
+  }, [store, router]);
+
   return (
-    <>
-      <div>
-        <h1>settings</h1>
-      </div>
-    </>
+    <S.Container>
+      <S.GoBackWrapper>
+        <Heading
+          pointer
+          type="h2"
+          weight="600"
+          color="tertiary"
+          size={isMobile ? 0.6 : 1}
+          onClick={() => router("/dashboard")}
+        >
+          Voltar
+        </Heading>
+      </S.GoBackWrapper>
+
+      <S.TextWrapper>
+        <Heading
+          type="h2"
+          weight="500"
+          color="primary"
+          size={isMobile ? 1.2 : 1.8}
+        >
+          <>
+            Atualize seus <strong>dados</strong>
+          </>
+        </Heading>
+      </S.TextWrapper>
+
+      <UpdateAccountForm />
+
+      <S.FinishAccountWrapper>
+        <Heading
+          pointer
+          type="h3"
+          weight="600"
+          color="primary"
+          size={isMobile ? 1 : 1.2}
+          onClick={() => handleDeleteAccount()}
+        >
+          Encerrar conta
+        </Heading>
+      </S.FinishAccountWrapper>
+    </S.Container>
   );
 };
 
---
+++ src/pages/Dashboard/Settings/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px 0;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.background};
+
+  @media screen and (min-width: 1281px) {
+    justify-content: flex-start;
+    height: calc(100vh - 180px);
+  }
+`;
+
+const GoBackWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  padding-bottom: 10px;
+`;
+
+const TextWrapper = styled.div`
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 1281px) {
+    margin: 30px 0;
+  }
+`;
+
+const FinishAccountWrapper = styled.div`
+  margin-top: 20px;
+
+  @media screen and (min-width: 1281px) {
+    margin-top: 30px;
+  }
+`;
+
+export { Container, GoBackWrapper, TextWrapper, FinishAccountWrapper };
