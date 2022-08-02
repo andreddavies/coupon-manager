4b31c515b44f0c6db8ac26dc71456b6a090ded19
Andre <andreddavies@hotmail.com>
2022-08-02 15:29:05 -0300

    Adding methods to coupon form

--- src/components/CouponForm/index.tsx
+++ src/components/CouponForm/index.tsx
@@ -1,70 +1,150 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import axios, { AxiosError } from "axios";
+import { useSelector } from "react-redux";
 
 import Form from "../Form";
 import Input from "../Input";
+import Toggle from "../Toggle";
+import Paragraph from "../Paragraph";
+import InputFile from "../InputFile";
 
-import { useDeviceDetect } from "../../hooks/useDeviceDetect";
+import { RootState } from "../../store";
+import { useMasks } from "../../hooks/useMasks";
+import CouponAPI from "../../services/CouponAPI";
+import { ICreateCouponReq } from "../../interfaces/ICoupon";
 
 import * as S from "./styles";
 import { REQUIRED_VALIDATION } from "../../constants/validations";
 
 type Props = {
   formType: "create" | "update";
+  updateData?: {
+    _id: string;
+    link: string;
+    logo: string;
+    code: string;
+    due_date: Date;
+    description: string;
+    company_name: string;
+    discount_percentage: number;
+    status: "active" | "inactive";
+  };
 };
 
-const CouponForm = ({ formType }: Props): React.ReactElement => {
-  const router = useNavigate();
-  const { isMobile } = useDeviceDetect();
+const CouponForm = ({ formType, updateData }: Props): React.ReactElement => {
+  const store = useSelector((state: RootState) => state);
 
-  const [code, setCode] = useState<string>("");
-  const [link, setLink] = useState<string>("");
-  const [dueDate, setDueDate] = useState<string>("");
-  const [status, setStatus] = useState<boolean>(false);
-  const [companyName, setCompanyName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
+  const [status, setStatus] = useState<boolean>(
+    updateData?.status === "active"
+  );
+  const [dueDate, setDueDate] = useState<string>(
+    updateData?.due_date
+      .toLocaleString("pt-BR")
+      .split("T")[0]
+      .split("-")
+      .reverse()
+      .join("-") || ""
+  );
+  const [companyName, setCompanyName] = useState<string>(
+    updateData?.company_name || ""
+  );
+  const [description, setDescription] = useState<string>(
+    updateData?.description || ""
+  );
+  const [link, setLink] = useState<string>(updateData?.link || "");
+  const [code, setCode] = useState<string>(updateData?.code || "");
   const [companyLogo, setCompanyLogo] = useState<File | FileList>();
-  const [discountPercentage, setDiscountPercentage] = useState<string>("");
+  const [discountPercentage, setDiscountPercentage] = useState<string>(
+    updateData?.discount_percentage.toString() || ""
+  );
+
+  const dueDateMasked = useMasks().useDate(dueDate);
 
   const [isCodeValid, setIsCodeValid] = useState<boolean>(false);
   const [isLinkValid, setIsLinkValid] = useState<boolean>(false);
   const [isDiscountPercentageValid, setIsDiscountPercentageValid] =
     useState<boolean>(false);
-  const [isStatusValid, setIsStatusValid] = useState<boolean>(false);
   const [isDueDateValid, setIsDueDateValid] = useState<boolean>(false);
   const [isCompanyNameValid, setIsCompanyNameValid] = useState<boolean>(false);
   const [isDescriptionValid, setIsDescriptionValid] = useState<boolean>(false);
-  const [isCompanyLogoValid, setIsCompanyLogoValid] = useState<boolean>(false);
 
   const [isFormValid, setIsFormValid] = useState<boolean>(false);
+
+  const handleChangeCapture = (event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    const file = (target.files as FileList)[0];
+
+    if (!!file && file.type.substring(0, 5) === "image") {
+      setCompanyLogo((target.files as FileList)[0] as File);
+    }
+  };
 
   const handleSubmit = async (
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
 
-    await console.log("submit");
+    const reverseDueDate = dueDate.split("-").reverse().join("-");
+
+    const userData = {
+      code,
+      link,
+      description,
+      logo: companyLogo as File,
+      company_name: companyName,
+      due_date: reverseDueDate,
+      status: status ? "active" : "inactive",
+      discount_percentage: parseInt(discountPercentage),
+    };
+
+    try {
+      const data = await CouponAPI[formType === "create" ? "create" : "update"](
+        { ...(userData as ICreateCouponReq), id: updateData?._id as string },
+        store.auth.token!
+      );
+
+      if (data) {
+        alert(
+          formType === "create"
+            ? "Cupom criado com sucesso!"
+            : "Cupom atualizado com sucesso!"
+        );
+        window.scrollTo({ top: 0 });
+        window.location.reload();
+      }
+    } catch (err) {
+      const errors = err as Error | AxiosError;
+
+      if (axios.isAxiosError(errors)) {
+        if (!errors.response) throw new Error("Erro inesperado!");
+        else
+          alert(
+            (errors.response?.data as { message: string }).message as string
+          );
+      }
+    }
   };
 
   useEffect(() => {
     setIsFormValid(
-      isCodeValid &&
-        isLinkValid &&
-        isStatusValid &&
-        isDueDateValid &&
-        isCompanyNameValid &&
-        isDescriptionValid &&
-        isCompanyLogoValid &&
-        isDiscountPercentageValid
+      formType === "update"
+        ? true
+        : isCodeValid &&
+            isLinkValid &&
+            !!companyLogo &&
+            isDueDateValid &&
+            isCompanyNameValid &&
+            isDescriptionValid &&
+            isDiscountPercentageValid
     );
   }, [
+    formType,
     isCodeValid,
     isLinkValid,
-    isStatusValid,
+    companyLogo,
     isDueDateValid,
     isCompanyNameValid,
     isDescriptionValid,
-    isCompanyLogoValid,
     isDiscountPercentageValid,
   ]);
 
@@ -73,6 +153,7 @@ const CouponForm = ({ formType }: Props): React.ReactElement => {
       id="couponForm"
       onSubmit={handleSubmit}
       buttonDisabled={!isFormValid}
+      encType="multipart/form-data"
       buttonText={formType === "create" ? "Criar" : "Atualizar"}
     >
       <Input
@@ -127,7 +208,8 @@ const CouponForm = ({ formType }: Props): React.ReactElement => {
         width="80%"
         type="text"
         id="dueDate"
-        value={dueDate}
+        maxLength={10}
+        value={dueDateMasked}
         marginVertical={0.25}
         label="Validade do cupom"
         validationCriteria={[REQUIRED_VALIDATION]}
@@ -135,33 +217,35 @@ const CouponForm = ({ formType }: Props): React.ReactElement => {
         onChange={(event) => setDueDate(event.target.value)}
       />
 
-      {/* <Input // create toggle
-        width="80%"
-        type="text"
-        id="status"
-        value={status}
-        marginVertical={0.25}
-        label="Status do cupom"
-        validationCriteria={[REQUIRED_VALIDATION]}
-        isValid={(value) => setIsStatusValid(value)}
-        onChange={(event) => setStatus(Boolean(event.target.value))}
-      /> */}
+      <S.ToggleContainer>
+        <Paragraph
+          size={1}
+          weight="500"
+          color={store.theme === "light" ? "primary" : "secondary"}
+        >
+          Status:
+        </Paragraph>
 
-      {/* <Input // create input type file
+        <S.ToggleWrapper>
+          <Toggle
+            id="status"
+            value={status}
+            onChange={() => setStatus(!status)}
+          />
+
+          <S.Span>{status ? "Ativo" : "Inativo"}</S.Span>
+        </S.ToggleWrapper>
+      </S.ToggleContainer>
+
+      <InputFile
         width="80%"
-        type="text"
-        id="companyLogo"
-        value={companyLogo}
-        marginVertical={0.25}
-        label="Logo da empresa. Clique aqui"
-        validationCriteria={[REQUIRED_VALIDATION]}
-        isValid={(value) => setIsCompanyLogoValid(value)}
-        onChange={(event) => setCompanyLogo(event.target.value)}
-      /> */}
+        id="couponImage"
+        onChangeCapture={handleChangeCapture}
+      />
 
       <Input
         width="80%"
-        type="text"
+        type="number"
         marginVertical={0.25}
         id="discountPercentage"
         value={discountPercentage}
--- src/components/CouponForm/styles.ts
+++ src/components/CouponForm/styles.ts
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export {};
+const ToggleContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ToggleWrapper = styled.div`
+  width: 100%;
+  height: 66px;
+  display: flex;
+  margin: 0.25rem 0;
+  align-items: center;
+`;
+
+const Span = styled.span`
+  margin-left: 1rem;
+  color: ${({ theme }) => theme.secondary};
+`;
+
+export { ToggleContainer, ToggleWrapper, Span };
