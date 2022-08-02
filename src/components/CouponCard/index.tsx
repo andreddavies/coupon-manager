ffce84721cae90ced51b9d660b39b4a78b25d766
Andre <andreddavies@hotmail.com>
2022-08-02 15:28:40 -0300

    Implementing final layout to coupon card

--- src/components/CouponCard/index.tsx
+++ src/components/CouponCard/index.tsx
@@ -1,11 +1,182 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { useSelector } from "react-redux";
+
+import Paragraph from "../Paragraph";
+import TrashIcon from "../../icons/TrashIcon";
+import PencilIcon from "../../icons/PencilIcon";
+
+import { RootState } from "../../store";
+import { useDeviceDetect } from "../../hooks/useDeviceDetect";
 
 import * as S from "./styles";
+import CouponAPI from "../../services/CouponAPI";
 
-const CouponCard = (): React.ReactElement => {
+type TProps = {
+  data: {
+    _id: string;
+    link: string;
+    logo: string;
+    code: string;
+    due_date: Date;
+    description: string;
+    company_name: string;
+    discount_percentage: number;
+    status: "active" | "inactive";
+  };
+  setModal: (value: "edit" | "delete") => void;
+};
+
+const CouponCard = ({ data, setModal }: TProps): React.ReactElement => {
+  const { isMobile } = useDeviceDetect();
+  const store = useSelector((state: RootState) => state);
+
+  const now = Date.now();
+  const due_date = new Date(
+    `${data.due_date.toString().split("T")[0]}T03:00:00.000Z`
+  ).getTime();
+  let difference = due_date - now;
+
+  const daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
+  difference -= daysDifference * 1000 * 60 * 60 * 24;
+  const hoursDifference = Math.floor(difference / 1000 / 60 / 60);
+  difference -= hoursDifference * 1000 * 60 * 60;
+  const minutesDifference = Math.floor(difference / 1000 / 60);
+  difference -= minutesDifference * 1000 * 60;
+  const [secondsDifference, setSecondsDifference] = useState<number>(0);
+
+  const urgent =
+    daysDifference === 0 &&
+    hoursDifference === 0 &&
+    minutesDifference === 0 &&
+    secondsDifference >= 0;
+
+  useEffect(() => {
+    if (
+      (urgent && minutesDifference && secondsDifference === 1) ||
+      daysDifference < 0
+    ) {
+      (async () => {
+        try {
+          const deleted = await CouponAPI.deleteCoupon(
+            data._id,
+            store.auth.token!
+          );
+
+          if (deleted) {
+            window.location.reload();
+          }
+        } catch (err) {
+          const errors = err as Error | AxiosError;
+
+          if (axios.isAxiosError(errors)) {
+            if (!errors.response) throw new Error("Erro inesperado!");
+            else
+              alert(
+                (errors.response?.data as { message: string }).message as string
+              );
+          }
+        }
+      })();
+    }
+
+    setTimeout(() => {
+      setSecondsDifference(Math.floor(difference / 1000));
+    }, 1000);
+  }, [secondsDifference]);
+
   return (
-    <S.Card>
-      <h3 style={{ color: "#fff" }}>Card</h3>
+    <S.Card urgent={urgent}>
+      <S.CardHeader>
+        <S.CompanyLogo
+          alt="Logo da Coupon Manager"
+          src={`http://localhost:4000/${data.logo}`}
+        />
+
+        <S.ClickableWrapper>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://${data.link}`}
+            style={{ fontSize: "0.7rem", color: urgent ? "#fff" : "#0000ff" }}
+          >
+            Ver cupom
+          </a>
+
+          <S.IconWrapper urgent={urgent} onClick={() => setModal("edit")}>
+            <PencilIcon
+              width={24}
+              height={24}
+              color={urgent ? "urgent" : "default"}
+            />
+          </S.IconWrapper>
+
+          <S.IconWrapper urgent={urgent} onClick={() => setModal("delete")}>
+            <TrashIcon
+              width={24}
+              height={24}
+              color={urgent ? "urgent" : "default"}
+            />
+          </S.IconWrapper>
+        </S.ClickableWrapper>
+      </S.CardHeader>
+
+      <S.InfoContainer>
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="600"
+          size={isMobile ? 1 : 1.8}
+        >
+          <>Código: {data.code}</>
+        </Paragraph>
+
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="500"
+          size={isMobile ? 0.8 : 1.8}
+        >
+          <>Descrição: {data.description}</>
+        </Paragraph>
+
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="600"
+          size={isMobile ? 0.8 : 1.8}
+        >
+          <>
+            {daysDifference} dias
+            <br />
+            {hoursDifference}:{String(minutesDifference).padStart(2, "0")}:
+            {String(secondsDifference).padStart(2, "0")}
+          </>
+        </Paragraph>
+
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="500"
+          size={isMobile ? 0.8 : 1.8}
+        >
+          <>
+            Nome da loja: <strong>{data.company_name}</strong>
+          </>
+        </Paragraph>
+
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="500"
+          size={isMobile ? 0.8 : 1.8}
+        >
+          <>{data.discount_percentage}%</>
+        </Paragraph>
+
+        <Paragraph
+          color={urgent ? "urgent" : "secondary"}
+          weight="600"
+          size={isMobile ? 0.8 : 1.8}
+        >
+          {data.status === "active" ? "Ativo" : "Inativo"}
+        </Paragraph>
+      </S.InfoContainer>
     </S.Card>
   );
 };
--- src/components/CouponCard/styles.ts
+++ src/components/CouponCard/styles.ts
@@ -1,13 +1,74 @@
 import styled from "styled-components";
 
-const Card = styled.div`
-  width: 80%;
-  height: 120px;
+type CardProps = {
+  urgent: boolean;
+};
+
+const Card = styled.div<CardProps>`
+  width: 90%;
+  display: flex;
   max-width: 980px;
+  max-height: 400px;
   margin: 0.75rem 0;
   padding: 20px 15px;
+  align-items: center;
   border-radius: 30px;
-  background-color: ${({ theme }) => theme.couponCard.background};
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${({ theme, urgent }) =>
+    theme.couponCard[urgent ? "featuredTime" : "background"]};
 `;
 
-export { Card };
+const CardHeader = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  justify-content: space-between;
+`;
+
+const CompanyLogo = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+`;
+
+const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  & > p {
+    margin: 0.3rem 0;
+  }
+`;
+
+const ClickableWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const IconWrapper = styled.button<CardProps>`
+  outline: 0;
+  width: 20px;
+  height: 20px;
+  border: none;
+  display: flex;
+  margin-left: 20px;
+  align-items: center;
+  background-color: ${({ theme, urgent }) =>
+    theme.couponCard[urgent ? "featuredTime" : "background"]};
+
+  :active {
+    opacity: 0.5;
+  }
+`;
+
+export {
+  Card,
+  CardHeader,
+  CompanyLogo,
+  InfoContainer,
+  ClickableWrapper,
+  IconWrapper,
+};
